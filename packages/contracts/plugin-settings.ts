d5e17acdd7de19c8f129f6417e54e7ada5d46dd4
emmdim <emmdim@users.noreply.github.com>
2024-01-26 14:23:57 +0100

    Testing deployment to goerli

--- packages/contracts/plugin-settings.ts
+++ packages/contracts/plugin-settings.ts
@@ -1,7 +1,7 @@
 import buildMetadata from './src/build-metadata.json';
 import releaseMetadata from './src/release-metadata.json';
 
-export const PLUGIN_REPO_ENS_NAME = 'vocdoni-gasless-voting-poc';
+export const PLUGIN_REPO_ENS_NAME = 'vocdoni-gasless-voting-poc-erc20';
 export const PLUGIN_CONTRACT_NAME = 'VocdoniVoting';
 export const PLUGIN_SETUP_CONTRACT_NAME = 'VocdoniVotingSetup';
 
