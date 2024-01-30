557d65515d78a9807c96402596be05af9c01f60c
emmdim <emmdim@users.noreply.github.com>
2024-01-30 12:25:01 +0100

    Updates plugin address for subgraph

--- packages/subgraph/utils/constants.ts
+++ packages/subgraph/utils/constants.ts
@@ -1,4 +1,4 @@
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
 
 export const PLUGIN_INTERFACE = '0x11223344';
-export const PLUGIN_REPO_ADDRESS = '0x74057f3f3809a874f28e3ebe80a1f1a5a137b64e'; // make sure address are lowercased
+export const PLUGIN_REPO_ADDRESS = '0x98c60a356f03244f9c33c9ca224d960cd45fd2c1'; // make sure address are lowercased
