7f3773f4bac646b86883ccf6d2b045dbf0945ed5
emmdim <emmdim@users.noreply.github.com>
2024-01-30 12:16:50 +0100

    New deployment setting minimum time for porposal to 5minutes

--- packages/contracts/plugin-settings.ts
+++ packages/contracts/plugin-settings.ts
@@ -1,13 +1,13 @@
 import buildMetadata from './src/build-metadata.json';
 import releaseMetadata from './src/release-metadata.json';
 
-export const PLUGIN_REPO_ENS_NAME = 'vocdoni-gasless-voting-poc-erc20';
+export const PLUGIN_REPO_ENS_NAME = 'vocdoni-gasless-voting-poc-vanilla-erc20';
 export const PLUGIN_CONTRACT_NAME = 'VocdoniVoting';
 export const PLUGIN_SETUP_CONTRACT_NAME = 'VocdoniVotingSetup';
 
 export const VERSION = {
   release: 1, // Increment this number ONLY if breaking/incompatible changes were made. Updates between releases are NOT possible.
-  build: 1, // Increment this number if non-breaking/compatible changes were made. Updates to newer builds are possible.
+  build: 2, // Increment this number if non-breaking/compatible changes were made. Updates to newer builds are possible.
 };
 
 // TODO: update the build metadata for decoding....
