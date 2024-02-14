3ffb071f4af5f13017afd49581adaacde44c25de
emmdim <emmdim@users.noreply.github.com>
2024-02-14 20:30:05 +0100

    Updates sepolia deployment and publishes ethers@0.0.1-rc3

--- packages/contracts/plugin-settings.ts
+++ packages/contracts/plugin-settings.ts
@@ -7,7 +7,7 @@ export const PLUGIN_SETUP_CONTRACT_NAME = 'VocdoniVotingSetup';
 
 export const VERSION = {
   release: 1, // Increment this number ONLY if breaking/incompatible changes were made. Updates between releases are NOT possible.
-  build: 2, // Increment this number if non-breaking/compatible changes were made. Updates to newer builds are possible.
+  build: 1, // Increment this number if non-breaking/compatible changes were made. Updates to newer builds are possible.
 };
 
 // TODO: update the build metadata for decoding....
