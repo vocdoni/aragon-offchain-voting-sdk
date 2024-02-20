bd7f937e0e7c803222962236cba3c2fd68e28791
emmdim <emmdim@users.noreply.github.com>
2024-02-20 16:50:22 +0100

    Fixes conversion of minProposerVotingPower returned by getVotingSettings to bigint

--- packages/js-client/src/internal/modules/methods.ts
+++ packages/js-client/src/internal/modules/methods.ts
@@ -400,6 +400,9 @@ export class GaslessVotingClientMethods
     plugins[0].minParticipation = decodeRatio(plugins[0].minParticipation, 6);
     plugins[0].supportThreshold = decodeRatio(plugins[0].supportThreshold, 6);
     plugins[0].proposalCount = Number(plugins[0].dao?.proposalsCount) || 0;
+    plugins[0].minProposerVotingPower = BigInt(
+      plugins[0].minProposerVotingPower
+    );
     return plugins[0] as GaslessPluginVotingSettings;
   }
 
